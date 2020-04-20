9d4849093ddd49ad8d553c927408f4b9313a697f
bonn2 <bonifas.fetzer@gmail.com>
2020-04-20 17:41:14 -0400

    Fixed TabCompletion

--- src/main/java/bonn2/deathitemsrewrite/MainTabComplete.java
+++ src/main/java/bonn2/deathitemsrewrite/MainTabComplete.java
@@ -1,32 +1,32 @@
 package bonn2.deathitemsrewrite;
 
-import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.command.TabCompleter;
 import org.bukkit.entity.Player;
 
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 
 public class MainTabComplete implements TabCompleter {
     @Override
     public List<String> onTabComplete(CommandSender sender, Command command, String alias, String[] args) {
+        Main plugin = Main.plugin;
         List<String> output = new ArrayList<>();
         switch (args.length) {
             case 0: {
-                return null;
+                for(Player p : plugin.getServer().getOnlinePlayers()) { output.add(p.getName()); }
+                return output;
             }
             case 1: {
-                Collection<? extends Player> players = Bukkit.getOnlinePlayers();
-                ArrayList<Player> playerArrayList = new ArrayList<>(players);
-                for (Player player : playerArrayList) {
-                    if (player.getName().startsWith(args[0])) {
-                        output.add(args[0]);
-                    }
+                for(Player p : plugin.getServer().getOnlinePlayers()) {
+                    if (p.getName().startsWith(args[0])) { output.add(p.getName()); }
                 }
                 return output;
+            }
+            case 2: {
+                int maxDeaths = plugin.getConfig().getInt("StoreDeaths");
+                for (int i = 1; i <= maxDeaths; i++) { output.add(i + ""); }
             }
             default: {
                 return output;
